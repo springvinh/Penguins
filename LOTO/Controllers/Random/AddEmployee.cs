4c9ee0c49ecb3fc38b584fcd820aaaf2993b0e14
Nguyen Ngoc Man Man <nguyenngocman1412@gmail.com>
2021-11-03 12:02:24 +0700

    Add RandomController getter stter

---
+++ LOTO/Controllers/Random/AddEmployee.cs
@@ -0,0 +1,12 @@
+﻿using LOTO.Models;
+
+namespace LOTO.Controllers.Random
+{
+    partial class RandomController
+    {
+        public void AddEmployee(int id, string name, string department)
+        {
+            Employees.Add(new Employee(id, name, department));
+        }
+    }
+}
---
+++ LOTO/Controllers/Random/AddSelectedEmployee.cs
@@ -0,0 +1,12 @@
+﻿using LOTO.Models;
+
+namespace LOTO.Controllers.Random
+{
+    partial class RandomController
+    {
+        public void AddSelectedEmployee(int id, string name, string department, string prize)
+        {
+            Selecteds.Add(new SelectedEmployee(id, name, department, prize)); 
+        }
+    }
+}
