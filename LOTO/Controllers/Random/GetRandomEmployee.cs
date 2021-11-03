ddc6afb2621eaf899463a8a5cad579f325f6b0e3
Nguyen Thanh Hoai <thanhhoai5683@gmail.com>
2021-11-03 12:08:41 +0700

    Add GetRandomEmployee func

---
+++ LOTO/Controllers/Random/GetRandomEmployee.cs
@@ -0,0 +1,35 @@
+﻿using LOTO.Models;
+using System;
+using System.Windows.Forms;
+
+namespace LOTO.Controllers.Random
+{
+    partial class RandomController
+    {
+        public string GetRandomEmployee(String prize)
+        {
+            System.Random random = new System.Random();
+            if (Employees.Count == 0)
+            {
+                MessageBox.Show("Danh sách nhân viên trống");
+            } else if (prize == "-- Chọn giải --")
+            {
+                MessageBox.Show("Chọn giải để quay");
+            } else
+            {
+                int randomIndex = random.Next(0, Employees.Count);
+                Employee employee = Employees[randomIndex];
+
+                AddSelectedEmployee(employee.Id, employee.Name, employee.Department, prize);
+                Employees.Remove(Employees[randomIndex]);
+
+                sourceEmployees.ResetBindings(false);
+                sourceSelected.ResetBindings(false);
+
+                return employee.Id.ToString();
+            }
+
+            return null;
+        }
+    }
+}
